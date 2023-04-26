f86c4d4010ec44a6d74ff0fc1fc9069afab85e62
UX-TO-DEV <113717673+shivyank121@users.noreply.github.com>
2023-04-27 01:06:44 +0530

    Updates

---
+++ precache-manifest.c4afd34febf7696cd3ed7e383c8d6692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2358fcfa876f66d070eb",
+    "url": "/ReactApp/static/css/main.ef6516a5.chunk.css"
+  },
+  {
+    "revision": "2358fcfa876f66d070eb",
+    "url": "/ReactApp/static/js/main.2358fcfa.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/ReactApp/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/ReactApp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "d1d0cad5fb783efab098",
+    "url": "/ReactApp/static/js/runtime~main.d1d0cad5.js"
+  },
+  {
+    "revision": "8e4728b9df472825da6d287cc49f08eb",
+    "url": "/ReactApp/index.html"
+  }
+];
